dc1e7ab1cd4172d4f7ace2dad31b583dca824887
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-09 12:22:33 +0300

    Fixed bugs in math metod

--- CurrencyExchange_ver2/RequestHandler.cs
+++ CurrencyExchange_ver2/RequestHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Humanizer;
+using WordsToNumbers;
 
 namespace CurrencyExchange_ver2
 {
@@ -10,18 +11,21 @@ namespace CurrencyExchange_ver2
         public string Handler(string value)
         {            
             if (Char.IsDigit(value[0])) return value;
-            //value = value.Replace(" ", "");
             return FromText(value);            
         }
 
         public string FromText(string value)
         {
             string currencyType = value.Substring(value.Length - 4);
-            Console.WriteLine("Currency type: " + currencyType);
             value = value.Remove(value.Length - 4);
-            string handledString = WordsToNumbers.ConvertToNumbers(value) + currencyType;
+
+            long numderFromString = WordsToNumbers.ConvertToNumbers(value);
+
+            string handledString = numderFromString + " " + currencyType;
 
             return handledString;
         }
+
+
     }
 }
