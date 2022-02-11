86dfe60252f67b05c2b9520d73d3c7d830cd1b6b
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-11 21:11:17 +0300

    TASK-3: Created candidate print report

--- CandidatesAndEmployees/CandidateReportGenerator.cs
+++ CandidatesAndEmployees/CandidateReportGenerator.cs
@@ -24,8 +24,17 @@ namespace CandidatesAndEmployees
 
         private void Print(List<Candidate> candidates)
         {
+            Console.WriteLine(("").PadRight(123, '-'));
+            Console.WriteLine("{0} {1,22 } {0,20} {2,15} {0,7} {3, 23} {0,14} {4,10} {0,3}", "|", "UserId",  "Full name", 
+                "Job title", "Salary");
+            Console.WriteLine(("").PadRight(123, '-'));
             foreach (var candidate in candidates)
-                candidate.DisplayInformation();
+            {
+                var fullName = $"{candidate.Name} {candidate.Surname}";
+                var salary = $"$ {candidate.JobSalary}";
+                Console.WriteLine($"|\t{candidate.Id,18}|\t{fullName,20}|\t{candidate.JobTitle,35}|\t{salary,10}|");
+            }
+            Console.WriteLine(("").PadRight(123, '-'));
         }
 
         private List<Candidate> SortBySalary(List<Candidate> candidates)
