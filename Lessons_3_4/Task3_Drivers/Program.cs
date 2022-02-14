fa20248a029597e6fa81d430765dff7931ef486b
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-14 14:57:40 +0300

    Created Task3_Drivers project

---
+++ Lessons_3_4/Task3_Drivers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task3_Drivers;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var faker = new Faker();
+            
+        DateTime startDate = DateTime.Parse("10/01/1970");
+        DateTime finishDate = DateTime.Parse("10/01/2022");
+        DateTime date;
+
+        date = faker.Date.BetweenOffset(
+            new DateTimeOffset(startDate), new DateTimeOffset(finishDate)).Date;
+
+    }
+}
