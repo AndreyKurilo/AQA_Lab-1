72661981c79ba0d8b2144ca371d6ba4bae483fb2
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-15 16:01:14 +0300

    Added Print metod to Driver.cs

--- Lessons_3_4/Task3_Drivers/Model/Driver.cs
+++ Lessons_3_4/Task3_Drivers/Model/Driver.cs
@@ -4,6 +4,13 @@ public class Driver : Person
 {
     public DateTime DateDriverLicense { get; set; }
     public Guid LicenseID { get; set; }
-    
-    
+
+    public void PrintDriver(Driver driver)
+    {
+        Console.WriteLine("Surname: " + driver.Surname);
+        Console.WriteLine("Name: " + driver.Name);
+        Console.WriteLine("Age: " + (DateTime.Now.Year - driver.DateOfBirth.Year));
+        Console.WriteLine("Date of release driver's license: " + driver.DateDriverLicense);
+        Console.WriteLine("License ID: " + driver.LicenseID);
+    }
 }
