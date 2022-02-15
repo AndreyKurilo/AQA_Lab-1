c21d83880bded7615253626e6ca44ae5427b6c3b
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-15 13:39:56 +0300

    Changes to Programm

---
+++ Lessons_3_4/Task3_Drivers/Menu.cs
@@ -0,0 +1,15 @@
+﻿namespace Task3_Drivers;
+
+public class Menu
+{
+    public int AskDriversNumber()
+    {
+        int numberOfDrivers;
+        
+        Console.WriteLine("Enter number of drivers");
+        numberOfDrivers = Int32.Parse(Console.ReadLine());
+        return numberOfDrivers;
+    }
+    
+    
+}
