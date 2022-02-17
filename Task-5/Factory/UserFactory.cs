e540aa8f1f0f502382699b56c58b7f392ce90352
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-17 21:20:39 +0300

    Factory for User created

---
+++ Task-5/Factory/UserFactory.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using Store.Model;
+
+namespace Store.Factory;
+
+public class UserFactory
+{
+    private readonly Faker _faker;
+
+    public UserFactory()
+    {
+        _faker = new Faker();
+    }
+
+    public User Create()
+    {
+        var user = new User()
+        {
+            PassportId = Guid.NewGuid(),
+            FullName = _faker.Name.FullName(),
+            Age = new Random().Next(10, 100)
+        };
+        return user;
+    }
+}
