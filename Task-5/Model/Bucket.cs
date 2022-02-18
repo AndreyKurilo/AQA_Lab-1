788b5b77930a26786d066517d5cd567dca2504b9
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-18 16:37:53 +0300

    added Add metod to Bucket

--- Task-5/Model/Bucket.cs
+++ Task-5/Model/Bucket.cs
@@ -1,12 +1,21 @@
-﻿namespace Store.Model;
+﻿using Store.Factory;
+
+namespace Store.Model;
 
 public class Bucket
 {
     public User User { get; set; }
-    public List<Product> goods { get; set; }
+    private List<Product> _goods;
+    private Product _product;
+
+    public Bucket()
+    {
+        _goods = new List<Product>();
+        var _product = new ProductFactory();
+    }
 
     public void AddProduct()
     {
-        
+        _goods.Add(_product);
     }
 }
