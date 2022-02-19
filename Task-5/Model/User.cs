a6b146e7f2d874bc459ef46e39c39c840b7ce7c9
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-19 14:52:42 +0300

    Override Equals metod for comparing users by ID

--- Task-5/Model/User.cs
+++ Task-5/Model/User.cs
@@ -3,6 +3,26 @@
 public class User
 {
     public Guid PassportId { get; set; }
+
+    private sealed class PassportIdEqualityComparer : IEqualityComparer<User>
+    {
+        public bool Equals(User x, User y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.PassportId.Equals(y.PassportId);
+        }
+
+        public int GetHashCode(User obj)
+        {
+            return obj.PassportId.GetHashCode();
+        }
+    }
+
+    public static IEqualityComparer<User> PassportIdComparer { get; } = new PassportIdEqualityComparer();
+
     public string FullName { get; set; } = null!;
     public int Age { get; set; }
 }
