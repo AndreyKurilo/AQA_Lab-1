9c7eb808c1d0baf886f56f6825b301af6b1445b7
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-23 00:01:33 +0300

    Added json file and DTO classes

---
+++ Task-6-7/Task-6-7/Model/ShopDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_6_7.Model;
+
+[Serializable]
+public class ShopDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<PhoneDto> Phones { get; set; }
+}
---
+++ Task-6-7/Task-6-7/ShopsDto.cs
@@ -0,0 +1,8 @@
+﻿using Task_6_7.Model;
+
+namespace Task_6_7;
+
+public class ShopsDto
+{
+    public List<ShopDto> Shops { get; set; }
+}
