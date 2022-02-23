a15fa569ad9c9ac22d21a5a0f01f52ce1faad06b
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-23 19:06:23 +0300

    added method Handle to Menu

--- Task-6-7/Task-6-7/Menu.cs
+++ Task-6-7/Task-6-7/Menu.cs
@@ -1,6 +1,35 @@
-﻿namespace Task_6_7;
+﻿using Task_6_7.Phones.Models;
+using Task_6_7.Shop;
+using Task_6_7.Shop.Models;
+
+namespace Task_6_7;
 
 public class Menu
 {
-    
+    public void Handle(ShopsDto shopsDto, string userChoice)
+    {
+        var input = new Input();
+        var output = new Output();
+        var shopTools = new ShopsTools();
+        var command = Int32.Parse(userChoice);
+        var chosenModel = "some model";
+        var shopsList = new Dictionary<ShopDto, PhoneDto>();
+        switch (command)
+        {
+            case 1:
+                output.ShowShopsAssortment(shopsDto);
+                break;
+            case 2:
+                output.AvailablePnones(shopsDto.Shops);
+                break;
+            case 3:
+                chosenModel = input.GetPhoneModel();
+                shopsList = shopTools.GetPhonesByModel(shopsDto, chosenModel);
+                output.PrintFoundPhone(shopsList);
+                break;
+            case 4:
+                
+        }
+    }
+
 }
