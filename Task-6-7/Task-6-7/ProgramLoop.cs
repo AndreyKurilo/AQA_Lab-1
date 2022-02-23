fc93753d0081fa91055c06fd500e0456f8f63a21
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-23 19:05:04 +0300

    created 2 classes - tools for shop and loop

---
+++ Task-6-7/Task-6-7/ProgramLoop.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_6_7;
+
+public class ProgramLoop
+{
+    private readonly Output _output;
+    private readonly Input _input;
+
+    public ProgramLoop(Input input, Output output)
+    {
+        _input = input;
+        _output = output;
+    }
+    
+    public void Run()
+    {
+        while (true)
+        {
+            // menu.DisplayOptions();
+            // var command = menu.AskForCommand();
+            // menu.Handle(command);
+        }
+    }
+}
---
+++ Task-6-7/Task-6-7/Shop/ShopsTools.cs
@@ -0,0 +1,58 @@
+﻿using Task_6_7.Phones.Exceptions;
+using Task_6_7.Phones.Models;
+using Task_6_7.Shop.Models;
+
+namespace Task_6_7.Shop;
+
+public class ShopsTools
+{
+    private Output _output;
+
+    public ShopsTools
+    {
+        _output = new Output();
+    }
+
+    public Dictionary<ShopDto, PhoneDto> GetPhonesByModel(ShopsDto shops, string phoneModel)
+    {
+        var phonesInShops = new Dictionary<ShopDto, PhoneDto>();
+
+        foreach (ShopDto shop in shops.Shops)
+        {
+            foreach (PhoneDto phone in shop.Phones)
+            {
+                if (phone.Model == phoneModel)
+                {
+                    phonesInShops.Add(shop, phone);
+                    break;
+                }
+            }
+        }
+
+        if (phonesInShops.Count == 0)
+        {
+            throw new PhoneNotFoundException();
+        }
+        else
+        {
+            _output.PrintPhonesAvailability(phonesInShops);
+        }
+
+        return phonesInShops;
+    }
+
+    public bool BuyPhone(Dictionary<ShopDto, PhoneDto> shopsList, string shopName)
+    {
+        foreach (var shops in shopsList)
+        {
+            PhoneDto phone = shops.Value;
+            ShopDto shop = shops.Key;
+
+            if (shop.Name != shopName) continue;
+            Console.WriteLine($"Order for \"{phone.Model}\" total sum: {phone.Price} is made successfully!");
+            return true;
+        }
+
+        return false;
+    }
+}
