b42df7ea1466095c08d9ece204a086c9774d843d
AndreyKurilo <akurilo.aaa@gmail.com>
2022-02-23 14:56:57 +0300

    Updated Program

--- Task-6-7/Task-6-7/Program.cs
+++ Task-6-7/Task-6-7/Program.cs
@@ -6,33 +6,14 @@ public static class Program
 {
     public static void Main()
     {
-        var path = Path.Combine(Environment.CurrentDirectory, @"Data\", "appsettings.json");
-
-        string json;
+        var output = new Output();
+        const string filename = "appsettings.json";
+        var action = new DoThis();
         
-        try
-        {
-            json = File.ReadAllText(path);
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("File not found");
-            return;
-        }
-        catch (IOException)
-        {
-            Console.WriteLine("Something went wrong on file reading");
-            return;
-        }
-
-        var shopsDto = JsonNet.Deserialize<ShopsDto>(json);
-
-        foreach (ShopDto shopData in shopsDto.Shops)
-        {
-            foreach (PhoneDto phoneData in shopData.Phones)
-            {
-                Console.WriteLine($"Model: {phoneData.Model}  OStype: {phoneData.OperationSystemType}");
-            }
-        }
+        ShopsDto shopsDto = action.ReadJsonFile(filename);
+        
+        output.ShowShopsAssortment(shopsDto);
+        
+        output.AvailablePnones(shopsDto.Shops);
     }
 }
