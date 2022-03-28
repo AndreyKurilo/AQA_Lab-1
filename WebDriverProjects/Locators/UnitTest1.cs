96701d20d01d1490b9c79ba9df96f401d3f11541
Andrey <akurilo.aaa@gmail.com>
2022-03-29 00:31:00 +0300

    Added browser drivers to Resources, html file opens

--- WebDriverProjects/Locators/UnitTest1.cs
+++ WebDriverProjects/Locators/UnitTest1.cs
@@ -2,22 +2,29 @@ using System.IO;
 using System.Reflection;
 using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Locators;
 
 public class Tests
 {
     private IWebDriver _webDriver;
+    private string FullPathToFile;
+
     [SetUp]
     public void Setup()
     {
         var BasePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        var FullPathToFile = $"{BasePath}{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}";
+        FullPathToFile = $"{BasePath}{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}somehtml.html";
+        var fullPathToBrowserDriver = $"{BasePath}{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}";
+        _webDriver = new ChromeDriver(fullPathToBrowserDriver);
     }
 
     [Test]
     public void Test1()
     {
-        Assert.Pass();
+        _webDriver.Navigate().GoToUrl(FullPathToFile);
+        var element = _webDriver.FindElement(By.Id("1"));
+        Assert.NotNull(element);
     }
 }
