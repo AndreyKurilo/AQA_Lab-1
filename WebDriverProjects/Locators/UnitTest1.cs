9da14ba569caffc1cb0ab9ef0ae6fd49f1981675
Andrey <akurilo.aaa@gmail.com>
2022-03-29 13:31:31 +0300

    Tests for Id are green

--- WebDriverProjects/Locators/UnitTest1.cs
+++ WebDriverProjects/Locators/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Reflection;
 using NUnit.Framework;
@@ -11,20 +12,46 @@ public class Tests
     private IWebDriver _webDriver;
     private string FullPathToFile;
 
-    [SetUp]
-    public void Setup()
+    [OneTimeSetUp]
+    public void OneTimeSetup()
     {
+        // Вариант 1: через установку в папку Resources - правый клик по драйверу
+        // ->Properties->Build action = Content; Copy to output directory = Copy always
+        // Должно отобразиться в папке проекта bin (если не оттображается бин - сделать Build проекта)
         var BasePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         FullPathToFile = $"{BasePath}{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}somehtml.html";
         var fullPathToBrowserDriver = $"{BasePath}{Path.DirectorySeparatorChar}Resources{Path.DirectorySeparatorChar}";
         _webDriver = new ChromeDriver(fullPathToBrowserDriver);
+        _webDriver.Navigate().GoToUrl(FullPathToFile);
     }
 
     [Test]
-    public void Test1()
+    public void IdLocatorTest_1()
     {
-        _webDriver.Navigate().GoToUrl(FullPathToFile);
-        var element = _webDriver.FindElement(By.Id("1"));
-        Assert.NotNull(element);
+        //_webDriver.Navigate().GoToUrl(FullPathToFile);
+        var elementById_1 = _webDriver.FindElement(By.Id("1")).Text;
+        Assert.AreEqual(elementById_1, "Test");
     }
+
+    [Test]
+    public void IdLocatorTest_2()
+    {
+        var elementById_11 = _webDriver.FindElement(By.Id("11")).Text;
+        Assert.AreEqual(elementById_11, "Task");
+    }
+
+    [Test]
+    public void IdLocatorTest_3()
+    {
+        var elementById_123 = _webDriver.FindElement(By.Id("123")).Text;
+        Assert.AreEqual(elementById_123, "Locator");
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        _webDriver.Quit();
+    }
+
+
 }
