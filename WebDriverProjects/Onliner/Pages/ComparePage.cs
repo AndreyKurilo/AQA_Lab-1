21a487ede0d6849c5298ab741e32dbc0452b85bf
Andrey <akurilo.aaa@gmail.com>
2022-04-09 20:13:52 +0300

    Test nearly done (green), wait metod havn't implemented yet

--- WebDriverProjects/Onliner/Pages/ComparePage.cs
+++ WebDriverProjects/Onliner/Pages/ComparePage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.ObjectModel;
 using Onliner.Services;
 using OpenQA.Selenium;
 
@@ -11,6 +12,11 @@ public class ComparePage : PageBase
     private static readonly By DiagonalTvFieldLocator = By.XPath("//span[contains(.,'Диагональ экрана')]");
     private static readonly By DiagonalTvTipLocator = By.XPath("//*[@id='product-table']/tbody[5]/tr[4]/td[1]/div/span");
     private static readonly By DiagonalTvTipTextLocator = By.XPath("//p[contains(.,'Размер диагонали экрана в дюймах.')]");
+    //private static readonly By ChosenItemsLocator = By.ClassName("product-summary__figure");
+    private static readonly By ChosenItemsRemoveButtonLocator = 
+        By.CssSelector("tr.product-table__row.product-table__row_header.product-table__row_top>th.product-table__cell>div.product-table-cell-container>a[title]");
+    private static readonly By ChosenItemsOnPageLocator = 
+        By.CssSelector("tr.product-table__row.product-table__row_header.product-table__row_top>th.product-table__cell>div.product-table-cell-container");
 
     public ComparePage(IWebDriver webDriver, bool openPageByUrl) : base(webDriver, openPageByUrl)
     {
@@ -34,6 +40,11 @@ public class ComparePage : PageBase
         }
     }
     
+    public ReadOnlyCollection<IWebElement> GetItemsOnCompare_Page() =>
+        Driver.FindElements(ChosenItemsOnPageLocator);
+    public ReadOnlyCollection<IWebElement> GetRemoveButtonsOnCompare_Page() =>
+        Driver.FindElements(ChosenItemsRemoveButtonLocator);
+
     public IWebElement Title => WaitService.WaitElementIsExists(TitleLocator);
     public IWebElement DiagonalTvField => WaitService.WaitElementIsExists(DiagonalTvFieldLocator);
     public IWebElement DiagonalTvTip => WaitService.WaitElementIsExists(DiagonalTvTipLocator);
--- WebDriverProjects/Onliner/Tests/ChooseItemsAndCompare.cs
+++ WebDriverProjects/Onliner/Tests/ChooseItemsAndCompare.cs
@@ -12,6 +12,7 @@ public class ChooseItemsAndCompare : TestBase
     public void Test_ChooseItemsAndGotoComparisonPage()
     {
         var numberItemsToCompare = 2;
+        var numberItemsToRemoveFromComparison = 1;
         CatalogTVpage catalogTV = new CatalogTVpage(Driver, true);
 
         catalogTV.AddDefiniteNumberItemsToCompare(numberItemsToCompare);
@@ -24,5 +25,15 @@ public class ChooseItemsAndCompare : TestBase
         Assert.IsTrue(comparePage.DiagonalTvTipText.Displayed); 
         comparePage.DiagonalTvTip.Click();
         Assert.IsTrue(comparePage.IsTextTipWindowInvisible);
+        var chosenItemsCount = comparePage.GetItemsOnCompare_Page().Count;
+        Assert.AreEqual(numberItemsToCompare, chosenItemsCount);
+        for (int i = 0; i < numberItemsToRemoveFromComparison; i++)
+        {
+            comparePage.GetRemoveButtonsOnCompare_Page()[i].Click();
+        }
+
+        var itemsAfterRemove = comparePage.GetItemsOnCompare_Page().Count;
+        Assert.AreEqual(numberItemsToCompare - numberItemsToRemoveFromComparison, itemsAfterRemove);
+
     }
 }
